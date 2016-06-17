7e96e02e05b31bb983023f068b4655b4b15e9db6
yuanman <yuanman@asiainfo.com>
2016-06-17 14:58:05 +0800

    update

--- script/user_dubbo_start.sh
+++ script/user_dubbo_start.sh
@@ -6,6 +6,7 @@ done
 
 CLASSPATH="${CP}"
 CLASSPATH="${DUBBO_CONFIG_PATH}:${CLASSPATH}"
+export CLASSPATH
 START_CMD="${MEM_ARGS} -Dpaas.dubbo.registry.address=${DUBBO_REGISTRY_ADD} -Dpaas.dubbo.protocol.port=$DUBBO_PORT  ${JAVA_OPTIONS} com.ai.paas.ipaas.DubboServiceStart > /logs/iPaas-${DUBBO_SERVER_NAME}-${DUBBO_PORT}.log & 2 > 1 &"
 
 echo ${JAVA_HOME}
